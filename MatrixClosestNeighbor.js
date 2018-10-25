b9dfadcca8578d0a4088829bc25848805c0e71da
mgonnet <gonnet.matias@gmail.com>
2018-10-24 21:34:07 -0300

    Matrix closest neighbor (#1)
    * matrixClosestNeighbor
    
    * moved to specific folder
    
    * added exec time measure

---
+++ MatrixClosestNeighbor.js
@@ -0,0 +1,37 @@
+var matrixClosestNeighbor = function(x,y,neighbors){
+	let matrix = Array(x).fill().map(()=>Array(y).fill(-1));
+	let q = [...neighbors];
+	while(q.length>0){		
+		let node = q.shift();		
+		if(matrix[node.x][node.y]===-1){
+			let jumps = (typeof node.jumps === 'undefined' ? 0 : node.jumps);	
+			matrix[node.x][node.y] = jumps;
+			jumps=jumps+1;
+
+			if(node.x+1<x && matrix[node.x+1][node.y]===-1){
+				q.push({x: node.x+1, y: node.y, jumps: jumps});
+			}
+			if(node.x-1>=0 && matrix[node.x-1][node.y]===-1){
+				q.push({x: node.x-1, y: node.y, jumps: jumps});
+			}
+			if(node.y+1<y && matrix[node.x][node.y+1]===-1){
+				q.push({x: node.x, y: node.y+1, jumps: jumps});
+			}
+			if(node.y-1>=0 && matrix[node.x][node.y-1]===-1){
+				q.push({x: node.x, y: node.y-1, jumps: jumps});
+			}
+		}
+		
+
+	}
+
+	return matrix;
+}
+
+matrixClosesNeighborExecTime = function(x,y,neighbors){
+	console.time();
+	let result = matrixClosestNeighbor(x,y,neighbors);
+	console.timeEnd();		
+}
+
+let result = matrixClosesNeighborExecTime(2048,2048,[{x:1,y:1},{x:1322,y:459}])
