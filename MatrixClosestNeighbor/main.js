196dbce8ad3f2096ae987e4a5ac1fce603a798c7
mgonnet <gonnet.matias@gmail.com>
2018-12-02 23:24:10 -0300

    Bugfix: fix typo in MatrixClosestNeighbor

--- MatrixClosestNeighbor/main.js
+++ MatrixClosestNeighbor/main.js
@@ -4,6 +4,6 @@ let promptResponse = (message) => {
   console.table(message.data)
 }
 
-worker.addEventListner('message', promptResponse, false)
+worker.addEventListener('message', promptResponse, false)
 
 worker.postMessage({ x: 2048, y: 2048, interestPoints: [{ x: 1, y: 1 }, { x: 1322, y: 459 }] })
