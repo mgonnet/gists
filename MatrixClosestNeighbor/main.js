62b2968abb627fac41cc4400bd7b97bfca72deea
mgonnet <gonnet.matias@gmail.com>
2018-12-05 00:08:21 -0300

    color experiment

--- MatrixClosestNeighbor/main.js
+++ MatrixClosestNeighbor/main.js
@@ -3,6 +3,7 @@ let worker = new Worker('./MatrixClosestNeighbor.js')
 let interestPoints = []
 
 let promptResponse = (message) => {
+	document.querySelector('body').setAttribute('style','background-color: blue')
   let div = document.createElement('div')
   div.innerHTML=message.data
   let body = document.querySelector('body')
@@ -10,11 +11,13 @@ let promptResponse = (message) => {
 	body.removeChild(body.firstChild).remove()
   }
   body.appendChild(div)
+  document.querySelector('body').setAttribute('style','background-color: white')
 }
 
 worker.addEventListener('message', promptResponse, false)
 
 let onclick = (event) => {
+	document.querySelector('body').setAttribute('style','background-color: red')
 	let target = event.target
 	if(target.nodeName==="TD"){
 		let y = target.cellIndex
